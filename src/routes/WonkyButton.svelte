6f13b5a7f80a6fc4322404aa1539922757ded4c6
kalatkarin <149759898+kalatkarin@users.noreply.github.com>
2024-03-16 16:05:39 +0800

    feat(WonkyButton.svelte): added code from previous assignment

--- src/routes/WonkyButton.svelte
+++ src/routes/WonkyButton.svelte
@@ -1,2 +1,21 @@
-<!-- Replace this file with your submission to the previous assignment -->
-<p>Hello world!</p>
+<script>
+  export let value = 0;
+	let random = 0;
+
+	function handleClick() {
+		random = Math.floor(Math.random() * 10);
+		value += random;
+	}
+</script>
+
+<button on:click={handleClick}>
+	{#if value % 3 == 0 && value % 5 == 0}
+		{'FizzBuzz ' + value}
+	{:else if value % 3 == 0}
+		{'Fizz ' + value}
+	{:else if value % 5 == 0}
+		{'Buzz ' + value}
+	{:else}
+		{value}
+	{/if}
+</button>
