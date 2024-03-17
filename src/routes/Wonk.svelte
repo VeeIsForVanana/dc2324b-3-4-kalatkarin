19d623e46611ddfcf2c81e54825d66826c2512f2
kalatkarin <149759898+kalatkarin@users.noreply.github.com>
2024-03-17 19:21:38 +0800

    feat(Wonk.svelte):

--- src/routes/Wonk.svelte
+++ src/routes/Wonk.svelte
@@ -1,4 +1,36 @@
 <script>
     import WonkyButton from './WonkyButton.svelte';
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let button_values = [
+        {value: 0},
+        {value: 0},
+        {value: 0},
+        {value: 0},
+        {value: 0},
+    ]
+
+    function fizzBuzz() {
+        let sum = 0;
+
+        for (let i = 0; i < button_values.length; i++) {
+            sum += button_values[i].value;
+        }
+
+        if (sum % 3 == 0 && sum % 5 == 0) {
+            dispatch('fizzbuzz', sum);
+        }
+        else if (sum % 3 == 0) {
+            dispatch('fizz', sum);
+        }
+        else if (sum % 5 == 0) {
+            dispatch('buzz', sum);
+        }
+    }
 </script>
-<!-- TODO: Your Solution -->
+
+{#each button_values as b}
+    <WonkyButton on:updateSum={fizzBuzz} bind:value={b.value}/>
+{/each}
